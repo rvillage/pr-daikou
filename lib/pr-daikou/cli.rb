24a06f1ef6ddd10d24e5923ac9e052710887e0cc
rvillage <rvillage@gmail.com>
2018-06-01 14:42:40 +0900

    :shirt: Fix lint

--- lib/pr-daikou/cli.rb
+++ lib/pr-daikou/cli.rb
@@ -28,18 +28,18 @@ module PRDaikou
 
     private
 
-      def parser
-        @parser ||= OptionParser.new do |opt|
-          opt.banner = 'Usage: pr-daikou [options]'
+    def parser
+      @parser ||= OptionParser.new do |opt|
+        opt.banner = 'Usage: pr-daikou [options]'
 
-          opt.on('--email EMAIL', "git committed user email, default: #{@options[:email]}") {|v| @options[:email] = v }
-          opt.on('--name NAME', "git committed user name, default: #{@options[:name]}") {|v| @options[:name] = v }
-          opt.on('-T', '--title TITLE', "pull request title, default: #{@options[:title]} [at Mon Jan 1 12:34:56 UTC 2017]") {|v| @options[:title] = v }
-          opt.on('--description DESC', "pull request description, default: #{@options[:description]}") {|v| @options[:description] = v }
-          opt.on('-m', '--commit MESSAGE', "add git commit message, default: #{@options[:commit]}") {|v| @options[:commit] = v }
-          opt.on('-b', '--base BRANCH', "pull request base branch, default: #{@options[:base]}") {|v| @options[:base] = v }
-          opt.on('-t', '--topic BRANCH', "create new branch, default: #{@options[:topic]}_[20170101123456.000]") {|v| @options[:topic] = v }
-        end
+        opt.on('--email EMAIL', "git committed user email, default: #{@options[:email]}") {|v| @options[:email] = v }
+        opt.on('--name NAME', "git committed user name, default: #{@options[:name]}") {|v| @options[:name] = v }
+        opt.on('-T', '--title TITLE', "pull request title, default: #{@options[:title]} [at Mon Jan 1 12:34:56 UTC 2017]") {|v| @options[:title] = v }
+        opt.on('--description DESC', "pull request description, default: #{@options[:description]}") {|v| @options[:description] = v }
+        opt.on('-m', '--commit MESSAGE', "add git commit message, default: #{@options[:commit]}") {|v| @options[:commit] = v }
+        opt.on('-b', '--base BRANCH', "pull request base branch, default: #{@options[:base]}") {|v| @options[:base] = v }
+        opt.on('-t', '--topic BRANCH', "create new branch, default: #{@options[:topic]}_[20170101123456.000]") {|v| @options[:topic] = v }
       end
+    end
   end
 end
